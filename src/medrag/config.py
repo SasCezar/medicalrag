e84550f6a084eca6f0de3f8e3ce6068df4abcae7
Cezar Sas <cezar.sas@gmail.com>
2025-11-20 09:26:33 +0100

    Adjusted a bit

--- src/medrag/config.py
+++ src/medrag/config.py
@@ -2,7 +2,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", override=True)
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        override=True,
+        extra="ignore",
+    )
 
     OPENAI_API_KEY: str
 
